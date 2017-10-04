73868ac452f65c5ae271e4b66789f4104128a063
whyboris <byakubchik@forbes.com>
2017-10-04 17:46:51 -0400

    css layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,10 @@ export class AppComponent {
 
     if (electronService.isElectron()) {
       console.log('Mode electron');
-      // Check if electron is correctly injected (see externals in webpack.config.js)
-      console.log('c', electronService.ipcRenderer);
-      // Check if nodeJs childProcess is correctly injected (see externals in webpack.config.js)
-      console.log('c', electronService.childProcess);
+      // // Check if electron is correctly injected (see externals in webpack.config.js)
+      // console.log('c', electronService.ipcRenderer);
+      // // Check if nodeJs childProcess is correctly injected (see externals in webpack.config.js)
+      // console.log('c', electronService.childProcess);
     } else {
       console.log('Mode web');
     }
