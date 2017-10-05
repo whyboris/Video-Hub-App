2bc92a27aa100154cefa82a472252c027267c23e
whyboris <byakubchik@forbes.com>
2017-10-05 18:35:42 -0400

    added interface

---
+++ src/app/components/common/final-array.interface.ts
@@ -0,0 +1,23 @@
+export interface FinalArray {
+  inputDir: string;   // later may support array of many input directories
+  outputDir: string;  // always just one -- for screenshots and JSON file
+  images: Array<any>; // see below
+}
+
+/*
+Each item in the images array is formatted thus:
+
+  [
+    'partial path to file',    <--- for opening the file, just prepend the `inputDir`
+    'full original file name', <--- for opening the file
+    'file name cleaned of dots, underscores, and file extension' <--- for searching
+    [
+      'full-path-and-filename-to-1st-screenshot',
+      'full-path-and-filename-to-2nd-screenshot',
+      'full-path-and-filename-to-3rd-screenshot',
+      'full-path-and-filename-to-4th-screenshot',
+      'full-path-and-filename-to-5th-screenshot', // currently only 5, later up to 10.
+    ]
+  ]
+
+*/
