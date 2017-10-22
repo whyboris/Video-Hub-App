844ed2c54eff9bf72b75c9809a17d863f1b557de
whyboris <byakubchik@forbes.com>
2017-10-22 09:07:52 -0400

    filmstrip shows 10 and correct hover behavior

--- src/app/components/home/filmstrip.component.ts
+++ src/app/components/home/filmstrip.component.ts
@@ -11,19 +11,29 @@ export class FilmstripComponent implements OnInit {
   @Input() stuff: any;
   @Input() folderPath: string;
   @Input() width: number;
+  indexArray: Array<number> = []; // to set z-index on css
 
-  constructor(public sanitizer: DomSanitizer) {}
+  constructor(
+    public sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit() {
 
     // hack -- populate hardcoded values -- fix later
     const fileNumber = this.stuff;
     this.stuff = [];
-    this.stuff[0] = 'boris/' + fileNumber + '-1.jpg';
-    this.stuff[1] = 'boris/' + fileNumber + '-2.jpg';
-    this.stuff[2] = 'boris/' + fileNumber + '-3.jpg';
-    this.stuff[3] = 'boris/' + fileNumber + '-4.jpg';
-    this.stuff[4] = 'boris/' + fileNumber + '-5.jpg';
+
+    for (let i = 0; i < 10; i++) {
+      this.stuff[i] = 'boris/' + fileNumber + '-' + (i + 1) + '.jpg';
+      this.indexArray[i] = 10 - i;
+    }
+  }
+
+  showThisOne(screen: number): void {
+    this.indexArray.forEach((element, index) => {
+      const distance = Math.abs(index - screen);
+      this.indexArray[index] = 10 - distance;
+    });
   }
 
 }
