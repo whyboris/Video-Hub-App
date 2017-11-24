b0bd8be47c2b29a7b460b09022977008fc7ecf2b
Boris <yboris@yahoo.com>
2017-11-24 16:29:39 -0500

    custom icons

--- src/app/components/common/settings-buttons.ts
+++ src/app/components/common/settings-buttons.ts
@@ -53,21 +53,21 @@ export let SettingsButtons = {
   'showThumbnails': {
     hidden: false,
     toggled: true,
-    iconName: 'icon-layout',
+    iconName: 'icon-show-thumbnails',
     title: 'Show thumbnails',
     description: 'Show thumbnails view',
   },
   'showFilmstrip': {
     hidden: false,
     toggled: false,
-    iconName: 'icon-menu',
+    iconName: 'icon-show-filmstrip',
     title: 'Show filmstrip',
     description: 'Show filmstrip view'
   },
   'showFiles': {
     hidden: false,
     toggled: false,
-    iconName: 'icon-menu',
+    iconName: 'icon-show-filenames',
     title: 'Show files',
     description: 'Show files view',
   },
@@ -81,21 +81,21 @@ export let SettingsButtons = {
   'fontSizeLarger': {
     hidden: false,
     toggled: false,
-    iconName: 'icon-resize-full',
+    iconName: 'icon-video-blank',
     title: 'Toggle font size',
     description: 'Make the font larger or smaller'
   },
   'hoverDisabled': {
     hidden: false,
     toggled: false,
-    iconName: 'icon-feather',
+    iconName: 'icon-toggle-scrub',
     title: 'Toggle hover animations',
     description: 'Scrolling over preview shows different screenshots'
   },
   'randomImage': {
     hidden: false,
     toggled: true,
-    iconName: 'icon-shuffle',
+    iconName: 'icon-video-blank',
     title: 'Show random screenshot',
     description: 'Show random screenshot in the preview'
   },
@@ -116,49 +116,49 @@ export let SettingsButtons = {
   'darkMode': {
     hidden: false,
     toggled: false,
-    iconName: 'icon-adjust',
+    iconName: 'icon-darken',
     title: 'Dark mode',
     description: 'Dark mode'
   },
   'folderUnion': {
     hidden: true,
     toggled: false,
-    iconName: 'icon-folder',
+    iconName: 'icon-folder-plus',
     title: 'Folder union search',
     description: 'Search in all folders containing any of the search words'
   },
   'folder': {
     hidden: false,
     toggled: false,
-    iconName: 'icon-folder',
+    iconName: 'icon-folder-plus',
     title: 'Folder search',
     description: 'Search in folders containing each of the search words'
   },
   'fileUnion': {
     hidden: true,
     toggled: false,
-    iconName: 'icon-video',
+    iconName: 'icon-video-plus',
     title: 'Video union search',
     description: 'Search for videos containing any of the search words'
   },
   'file': {
     hidden: false,
     toggled: true,
-    iconName: 'icon-video',
+    iconName: 'icon-video-blank',
     title: 'Video search',
     description: 'Search for videos containing each of the search words'
   },
   'exclude': {
     hidden: false,
     toggled: true,
-    iconName: 'icon-thumbs-down',
+    iconName: 'icon-video-x',
     title: 'Exclude filter',
     description: 'Exclude any files that contain this string'
   },
   'magic': {
     hidden: false,
     toggled: true,
-    iconName: 'icon-search',
+    iconName: 'icon-looking-glass',
     title: 'Magic search',
     description: 'Live search showing all files and files inside folders that contain search words'
   },
@@ -172,21 +172,21 @@ export let SettingsButtons = {
   'hideSidebar': {
     hidden: false,
     toggled: false,
-    iconName: 'icon-left-circled',
+    iconName: 'icon-video-blank',
     title: 'Hide sidebar',
     description: 'Hides the search filter sidebar'
   },
   'hideTop': {
     hidden: false,
     toggled: false,
-    iconName: 'icon-up-circled',
+    iconName: 'icon-video-blank',
     title: 'Hide top',
     description: 'Hide the top bar'
   },
   'flatIcons': {
     hidden: false,
     toggled: false,
-    iconName: 'icon-check', // this specific icon makes the setting only appear in All Settings (behind gear button)
+    iconName: 'icon-checkmark', // this specific icon makes the setting only appear in All Settings (behind gear button)
     title: 'Flat icons',
     description: 'Flat icon style'
   }
