fa3b309df3c79e38d9b7cd94542902d1a819f1a0
Boris <yboris@yahoo.com>
2018-12-27 09:47:02 -0500

    Remember window size and position (#41)
    * restore window position and size
    
    * cleanup
    
    * handle off-screen coordinates case

--- src/app/components/common/settings-object.interface.ts
+++ src/app/components/common/settings-object.interface.ts
@@ -1,8 +1,19 @@
+import { AppStateInterface } from './app-state';
+import { HistoryItem } from './history-item.interface';
+
 export type SupportedLanguage = 'en' | 'ru';
 
 export interface SettingsObject {
-  appState: any;
+  appState: AppStateInterface;
   buttonSettings: any;
   language: SupportedLanguage;
-  vhaFileHistory: any;
+  vhaFileHistory: HistoryItem[];
+  windowSizeAndPosition: WinBounds;
+}
+
+interface WinBounds {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
 }
