1d290e663aeb6ff18aac370a42553ad2a75fff3e
Boris <yboris@yahoo.com>
2018-12-29 10:30:10 -0500

    Statistics menu (#52)
    * statistics menu
    
    * translations

---
+++ src/app/components/home/statistics/statistics.component.ts
@@ -0,0 +1,46 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { ImageElement } from '../../common/final-object.interface';
+
+@Component({
+  selector: 'app-statistics',
+  templateUrl: './statistics.component.html',
+  styleUrls: ['./statistics.component.scss']
+})
+export class StatisticsComponent implements OnInit {
+
+  @Input() finalArray: ImageElement[];
+  @Input() numFolders: number;
+
+  totalFiles: number;
+
+  longest: number = 0;
+  shortest: number = Infinity;
+  totalLength: number = 0;
+  avgLength: number;
+
+  largest: number = 0;
+  smallest: number = Infinity;
+  totalSize: number = 0;
+  avgSize: number;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.finalArray.forEach((element: ImageElement): void => {
+      this.shortest = Math.min(element.duration, this.shortest);
+      this.longest = Math.max(element.duration, this.longest);
+      this.totalLength += element.duration;
+
+      this.smallest = Math.min(element.fileSize, this.smallest);
+      this.largest = Math.max(element.fileSize, this.largest);
+      this.totalSize += element.fileSize;
+    });
+
+    this.totalFiles = this.finalArray.length;
+
+    this.avgLength = Math.round(this.totalLength / this.totalFiles);
+    this.avgSize = Math.round(this.totalSize / this.totalFiles);
+  }
+
+}
--- src/app/components/pipes/file-size.pipe.ts
+++ src/app/components/pipes/file-size.pipe.ts
@@ -9,10 +9,11 @@ export class FileSizePipe implements PipeTransform {
    * Return size of file formatted as XXX{mb,gb}
    * @param sizeInBytes -- file size in bytes
    */
-  transform(sizeInBytes: number): string {
+  transform(sizeInBytes: number, excludeParen: boolean): string {
     if (sizeInBytes) {
-      const rounded = Math.round(sizeInBytes / 1000000.00);
-      return '(' + (rounded > 999 ? Math.round(rounded / 100) / 10 + 'gb' : rounded + 'mb') + ')';
+      const rounded = Math.round(sizeInBytes / 1000000);
+      // tslint:disable-next-line:max-line-length
+      return (excludeParen ? '' : '(') + (rounded > 999 ? Math.round(rounded / 100) / 10 + 'gb' : rounded + 'mb') + (excludeParen ? '' : ')');
     } else {
       return '';
     }
