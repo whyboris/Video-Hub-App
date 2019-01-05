b51d7d29f529b8d3a24a30cf723a2bd3d294ed30
Boris <yboris@yahoo.com>
2019-01-05 06:31:13 -0500

    auto detect language on first start (#78)

--- src/app/components/common/settings-object.interface.ts
+++ src/app/components/common/settings-object.interface.ts
@@ -1,12 +1,9 @@
 import { AppStateInterface } from './app-state';
 import { HistoryItem } from './history-item.interface';
 
-export type SupportedLanguage = 'en' | 'ru';
-
 export interface SettingsObject {
   appState: AppStateInterface;
   buttonSettings: any;
-  language: SupportedLanguage;
   vhaFileHistory: HistoryItem[];
   windowSizeAndPosition: WinBounds;
 }
