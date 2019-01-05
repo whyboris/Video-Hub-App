aac454c26ec57609fb72707495f609f0270634cc
cal2195 <calmart101@gmail.com>
2019-01-05 12:38:32 +0000

    Use node instead for literal string replacement!
    Also add fr.ts! :)

--- src/app/i18n/translate.sh
+++ src/app/i18n/translate.sh
@@ -11,10 +11,14 @@ read -p "Please copy to_translate.txt contents into Google Translate, and paste 
 # replace each line from to_translate.txt with each line in translate.txt in our new file
 # also update the const name!
 while read -r en && read -r tr <&3; do
-  # deal with d'em quotes!
-  tr=$(echo "$tr" | sed -e "s/'/\\\\\\\\'/g")
   # show what we're gonna do
   echo "$en -> $tr"
   # bam!
-  sed -i '' "s/'$en'/'$tr'/g;s/English/$NEW_LANG_NAME/g" $NEW_LANG_CODE.ts
+  export FNAME=$NEW_LANG_CODE.ts
+  export FIND=$en
+  export REPLACE=$tr
+  node -e "fs=require('fs');fs.readFile(process.env.FNAME,'utf8',(err,data)=>{if(err!=null)throw err;fs.writeFile(process.env.FNAME,data.replace('\\''+process.env.FIND+'\\'','\\''+process.env.REPLACE.split('\\'').join('\\\\\\'')+'\\''),'utf8',e=>{if(e!=null)throw e;});});"
 done < to_translate.txt 3< translated.txt
+export FIND="English"
+export REPLACE=$NEW_LANG_NAME
+node -e "fs=require('fs');fs.readFile(process.env.FNAME,'utf8',(err,data)=>{if(err!=null)throw err;fs.writeFile(process.env.FNAME,data.replace('\\''+process.env.FIND+'\\'','\\''+process.env.REPLACE.split('\\'').join('\\\\\\'')+'\\''),'utf8',e=>{if(e!=null)throw e;});});"
