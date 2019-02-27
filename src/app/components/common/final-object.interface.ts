e6446b0c6d67eb8d25f58fd02c29860370a13b1b
cal2195 <calmart101@gmail.com>
2019-02-27 16:50:13 +0000

    Fix star rating type!

--- src/app/components/common/final-object.interface.ts
+++ src/app/components/common/final-object.interface.ts
@@ -1,7 +1,7 @@
 import { ResolutionString } from '../pipes/resolution-filter.service';
 import { ScreenshotSettings } from '../../../../main-globals';
 
-export type StarRating = 0.5 | 1.5 | 2.5 | 3.5;
+export type StarRating = 0.5 | 1.5 | 2.5 | 3.5 | 4.5 | 5.5;
 
 export interface FinalObject {
   addTags?: string[];           // tags to add
