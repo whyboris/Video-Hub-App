37a1519ba0ddace2df2d8421e2dca8d4a9632658
whyboris <yboris@yahoo.com>
2019-08-22 17:10:46 -0400

    cosmetic

--- src/app/i18n/en.ts
+++ src/app/i18n/en.ts
@@ -63,7 +63,7 @@ export const English = {
     muteClipsDescription               :'Mute all sound',
     muteClipsHint                      :'Mute all sound',
     openAtTimestampDescription         :'Open file at particular time rather than from beginning in Thumbnails view',
-    openAtTimestampMoreInfo            :'Requires you manually set the default video player below. Works on Windows with VLC, MPC, and POT players only; on Mac only with LVC',
+    openAtTimestampMoreInfo            :'Requires you manually set the default video player below. Works on Windows with VLC, MPC, and POT players only; on Mac only with VLC',
     openAtTimestampHint                :'Open file at timestamp',
     regenerateLibraryDescription       :'Regenerate all library metadata, except thumbnails and user entered metadata',
     regenerateLibraryHint              :'Regenerate library',
