1ab95d752f9d70af870ebf1e315813a5cb40736a
Boris Yakubchik <yboris@yahoo.com>
2019-11-07 21:27:03 -0500

    double quote -> single quote :man_shrugging:

--- src/app/components/sheet/sheet.component.ts
+++ src/app/components/sheet/sheet.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, Output, OnInit, ElementRef, EventEmitter, ViewChild }
 import { DomSanitizer } from '@angular/platform-browser';
 
 import { ManualTagsService } from '../tags-manual/manual-tags.service';
-import { FilePathService } from "../views/file-path.service";
+import { FilePathService } from '../views/file-path.service';
 
 import { StarRating, ImageElement } from '../../../../interfaces/final-object.interface';
 
@@ -71,7 +71,7 @@ export class SheetComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.fullFilePath = this.filePathService.createFilePath(this.folderPath,this.hubName,"filmstrips",this.video.hash);
+    this.fullFilePath = this.filePathService.createFilePath(this.folderPath, this.hubName, 'filmstrips', this.video.hash);
     this.percentOffset = (100 / (this.video.screens - 1));
     this.starRatingHack = this.star;
   }
