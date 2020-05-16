c6d9a2fc83666b4de94df46e4d26579213181565
whyboris <yboris@yahoo.com>
2020-05-16 13:24:33 -0400

    iconName not required; defaults to icon-default-button

--- src/app/common/settings-buttons.interface.ts
+++ src/app/common/settings-buttons.interface.ts
@@ -1,7 +1,7 @@
 export interface SettingsButton {
   description: string;
   hidden: boolean;          // hidden from the buttons ribbon by default (eye closed icon in settings)
-  iconName: string;
+  iconName?: string;        // if absent, defaults to `icon-default-button`
   title: string;
   toggled: boolean;         // default state unless user overrides
   moreInfo?: string;        // hint text in the settings menu when hovering over the `i` icon
