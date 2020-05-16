1f257b1871d7e2aa163de71ce480ac1897f77cf9
whyboris <yboris@yahoo.com>
2020-05-16 12:21:34 -0400

    indicate when regex is malformed

--- src/app/pipes/regex-search.pipe.ts
+++ src/app/pipes/regex-search.pipe.ts
@@ -1,11 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 import { ImageElement } from '../../../interfaces/final-object.interface';
+import { PipeSideEffectService } from './pipe-side-effect.service';
 
 @Pipe({
   name: 'regexSearchPipe'
 })
 export class RegexSearchPipe implements PipeTransform {
+
+  constructor(
+    public pipeSideEffectService: PipeSideEffectService
+  ) {}
 
   /**
    * Return only items that match the regex
@@ -21,8 +26,10 @@ export class RegexSearchPipe implements PipeTransform {
 
       try {
         re = RegExp(regexString, 'i');
+        this.pipeSideEffectService.regexPipeError(false);
       } catch {
         re = RegExp('');
+        this.pipeSideEffectService.regexPipeError(true);
       }
 
       return finalArray.filter(item => {
