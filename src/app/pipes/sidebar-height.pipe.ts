4fbdcfecc2268c4fc77da956b7aa69a359c78441
whyboris <yboris@yahoo.com>
2020-05-16 13:09:32 -0400

    sidebar height pipe

---
+++ src/app/pipes/sidebar-height.pipe.ts
@@ -0,0 +1,28 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sidebarHeightPipe'
+})
+export class SidebarHeightPipe implements PipeTransform {
+
+  /**
+   * Return number of pixels to offset the sidebar (as a string)
+   * @param finalArray
+   * @param searchString  the search string
+   */
+  transform(
+    menuHidden: boolean,
+    hideTop: boolean,
+    showRelatedVideosTray: boolean,
+    showTagTray: boolean
+  ): string {
+
+    return (
+        (menuHidden                             ? -32 :  0)
+      + (hideTop                                ?  53 : 98)
+      + ((showRelatedVideosTray || showTagTray) ? 160 :  0)
+      ).toString();
+
+  }
+
+}
