ead55549c285653b6dabda18c913c87ac105c23c
whyboris <yboris@yahoo.com>
2020-08-14 19:58:09 -0400

    clip handle double click

--- src/app/components/views/clip/clip.component.ts
+++ src/app/components/views/clip/clip.component.ts
@@ -4,7 +4,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { FilePathService } from '../file-path.service';
 
 import { ImageElement } from '../../../../../interfaces/final-object.interface';
-import { RightClickEmit, VideoClickEmit, DoubleClickVideoEmit } from '../../../../../interfaces/shared-interfaces';
+import { RightClickEmit, VideoClickEmit } from '../../../../../interfaces/shared-interfaces';
 
 import { metaAppear, textAppear } from '../../../common/animations';
 
@@ -25,7 +25,6 @@ export class ClipComponent implements OnInit {
   @Output() rightClick = new EventEmitter<RightClickEmit>();
   @Output() sheetClick = new EventEmitter<any>(); // does not emit data of any kind
   @Output() videoClick = new EventEmitter<VideoClickEmit>();
-  @Output() doubleClick = new EventEmitter<DoubleClickVideoEmit>();
 
   @Input() video: ImageElement;
 
