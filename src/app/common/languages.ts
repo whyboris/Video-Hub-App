aacbbddf23f0166a11a8502d83f616d7854e6e96
whyboris <yboris@yahoo.com>
2020-11-19 10:31:47 -0500

    Polish language added

--- src/app/common/languages.ts
+++ src/app/common/languages.ts
@@ -3,6 +3,7 @@ const Arabic     = require('../../../i18n/ar.json');
 const Bengali    = require('../../../i18n/bn.json');
 const Chinese    = require('../../../i18n/zh.json');
 const Czech      = require('../../../i18n/cs.json');
+const Dutch      = require('../../../i18n/nl.json');
 const English    = require('../../../i18n/en.json');
 const French     = require('../../../i18n/fr.json');
 const German     = require('../../../i18n/de.json');
@@ -11,13 +12,13 @@ const Italian    = require('../../../i18n/it.json');
 const Japanese   = require('../../../i18n/ja.json');
 const Korean     = require('../../../i18n/ko.json');
 const Malay      = require('../../../i18n/ms.json');
+const Polish     = require('../../../i18n/pl.json');
 const Portuguese = require('../../../i18n/pt.json');
 const Russian    = require('../../../i18n/ru.json');
 const Spanish    = require('../../../i18n/es.json');
+const Turkish    = require('../../../i18n/tr.json');
 const Ukrainian  = require('../../../i18n/uk.json');
 const Vietnamese = require('../../../i18n/vi.json');
-const Turkish    = require('../../../i18n/tr.json');
-const Dutch      = require('../../../i18n/nl.json');
 
 export const LanguageLookup: Record<string, any> = {
   'ar': Arabic,
@@ -32,11 +33,12 @@ export const LanguageLookup: Record<string, any> = {
   'ja': Japanese,
   'ko': Korean,
   'ms': Malay,
+  'nl': Dutch,
+  'pl': Polish,
   'pt': Portuguese,
   'ru': Russian,
-  'uk': Ukrainian,
-  'zh': Chinese,
-  'vi': Vietnamese,
   'tr': Turkish,
-  'nl': Dutch
+  'uk': Ukrainian,
+  'vi': Vietnamese,
+  'zh': Chinese,
 };
