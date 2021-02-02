1dc15340914ccaa1b0ea56414594d0ff42ff54e9
John Leckrone <jleckron@ucsc.edu>
2021-02-02 15:56:25 -0800

    Switched fps ordering to better match with other ordering patterns

--- src/app/pipes/sorting.pipe.ts
+++ src/app/pipes/sorting.pipe.ts
@@ -300,11 +300,11 @@ export class SortingPipe implements PipeTransform {
       });
     } else if (sortingType === 'fpsAsc') {
       return galleryArray.slice().sort((x: ImageElement, y: ImageElement): any => {
-        return this.sortFunctionLol(x, y, 'frameRate', false);
+        return this.sortFunctionLol(x, y, 'frameRate', true);
       });
     } else if (sortingType === 'fpsDesc') {
       return galleryArray.slice().sort((x: ImageElement, y: ImageElement): any => {
-        return this.sortFunctionLol(x, y, 'frameRate', true);
+        return this.sortFunctionLol(x, y, 'frameRate', false);
       });
     }else {
       return galleryArray.slice().sort((x: ImageElement, y: ImageElement): any => {
