1240e024bb9af0be215e1cac9ff62199e7cca1eb
Boris Yakubchik <yboris@yahoo.com>
2021-10-20 12:04:50 -0400

    remove enableRemoteModule

--- main.ts
+++ main.ts
@@ -137,7 +137,6 @@ function createWindow() {
       nodeIntegration: true,
       allowRunningInsecureContent: true,
       contextIsolation: false,
-      enableRemoteModule: true,
       webSecurity: false  // allow files from hard disk to show up
     },
     x: mainWindowState.x,
