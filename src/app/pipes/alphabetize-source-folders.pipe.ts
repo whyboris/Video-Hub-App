bb85e3ec5a0c58265b8b04b82aea090041b0ed32
Boris <yboris@yahoo.com>
2023-02-20 18:23:47 -0500

    merge PR #703 & resolve conflicts (#797)
    Code written by @alzbuc 🙇

---
+++ src/app/pipes/alphabetize-source-folders.pipe.ts
@@ -0,0 +1,35 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'alphabetizeSourceFolders'
+})
+export class AlphabetizeSourceFoldersPipe implements PipeTransform {
+  transform(array: any[], field: string): any[] {
+
+    // If using multi-level selector or keyvalue pipe in foreach, you need to nest at a deeper level
+    const fields = field.split('.');
+
+    array.sort((a: any, b: any) => {
+
+      // Init strings
+      let s1, s2;
+      if (fields.length > 1) {
+        s1 = a[fields[0]][fields[1]];
+        s2 = b[fields[0]][fields[1]];
+      } else {
+        s1 = a[field];
+        s2 = b[field];
+      }
+
+      // Ordering logic
+      if (s1 < s2) {
+        return -1;
+      } else if (s1 > s2) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    return array;
+  }
+}
