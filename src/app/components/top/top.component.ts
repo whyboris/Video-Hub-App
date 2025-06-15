78227d4855e3ca1ac3b458fbfde98c59a9f5c9d8
Ammar Amin <aamin174@gmail.com>
2025-06-16 01:09:17 +0930

    add a tiny thumbnail to the left of the folder & filename

--- src/app/components/top/top.component.ts
+++ src/app/components/top/top.component.ts
@@ -1,10 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import type { ImageElement } from '../../../../interfaces/final-object.interface';
 
 @Component({
   selector: 'app-top-component',
   templateUrl: './top.component.html',
-  styleUrls: ['./top.component.scss',
-              '../../fonts/icons.scss']
+  styleUrls: ['./top.component.scss', '../../fonts/icons.scss']
 })
 export class TopComponent {
 
@@ -16,8 +16,7 @@ export class TopComponent {
     this._folder = (folderString && folderString.trim()) || '';
     this.folderNameArray = this._folder.split('/');
     this.folderNameArray = this.folderNameArray.filter((element, index) => {
-      // TODO -- fix this up:
-      return index === 0 || element !== ''; // ATROCIOUS hack! -- simply to prevent ["", ""]
+      return index === 0 || element !== '';
     });
   }
   get folderString(): string { return this._folder; }
@@ -30,6 +29,11 @@ export class TopComponent {
   }
   get fileString(): string { return this._file; }
 
+  // Inputs for thumbnail
+  @Input() currentClickedItem: ImageElement;
+  @Input() hubName: string;
+  @Input() selectedOutputFolder: string;
+
   @Output() onFileWordClicked = new EventEmitter<string>();
   @Output() onFolderWordClicked = new EventEmitter<string>();
   @Output() onOpenInExplorer = new EventEmitter<boolean>();
@@ -37,12 +41,21 @@ export class TopComponent {
   public folderNameArray: string[];
   public fileNameArray: string[];
 
+  /**
+   * Compute the thumbnail path for the selected file
+   */
+  getThumbnailPath(): string {
+    if (this.currentClickedItem && this.hubName && this.selectedOutputFolder && this.currentClickedItem.cleanName !== '*FOLDER*') {
+      return `file://${this.selectedOutputFolder}/vha-${this.hubName}/thumbnails/${this.currentClickedItem.hash}.jpg`;
+    }
+    return '';
+  }
+
   public folderWordClicked(item: string): void {
     this.onFolderWordClicked.emit(item.trim());
   }
 
   public fileWordClicked(item: string): void {
-    // Strip away any of: {}()[].,
     const regex = /{|}|\(|\)|\[|\]|\.|\,/g;
     item = item.replace(regex, '');
     this.onFileWordClicked.emit(item.trim());
@@ -51,5 +64,4 @@ export class TopComponent {
   public openInExplorer(): void {
     this.onOpenInExplorer.emit(true);
   }
-
 }
