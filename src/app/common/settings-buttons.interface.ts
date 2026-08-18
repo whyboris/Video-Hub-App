91d38dbfd52e6491d030520a877a54c80e97a7cc
Boris Yakubchik <yboris@yahoo.com>
2026-08-18 13:47:42 -0400

    set up buttons to toggle visibility of metadata in details view

--- src/app/common/settings-buttons.interface.ts
+++ src/app/common/settings-buttons.interface.ts
@@ -1,9 +1,10 @@
 export interface SettingsButton {
-  description: string;
-  hidden: boolean;          // hidden from the buttons ribbon by default (eye closed icon in settings)
-  iconName?: string;        // if absent, defaults to `icon-default-button`
   title: string;
+  description: string;
   toggled: boolean;         // default state unless user overrides
+  hidden: boolean;          // hidden from the buttons ribbon by default (eye closed icon in settings)
+  // Optional
+  iconName?: string;        // if absent, defaults to `icon-default-button`
   moreInfo?: string;        // hint text in the settings menu when hovering over the `i` icon
   settingsHeading?: string; // Long text to appear in the settings above the button
 }
